415cb07455f0b8830040a534afd46ff7a61328d4
saksham <sakshamkhatwani@gmail.com>
2019-08-11 15:18:30 +0530

    commiting second time

--- advanced-practise/src/context/contextPracice.js
+++ advanced-practise/src/context/contextPracice.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
-const ThemeContext = React.createContext('light');
+// const ThemeContext = React.createContext('light');
 
-class App extends React.Component{
-    render(){
-        return(
-            <ThemeContext.Provider value='light'>
-                <Toolbar/>
-            </ThemeContext.Provider>
-        );
-    }
-}
+// class App extends React.Component{
+//     render(){
+//         return(
+//             <ThemeContext.Provider value='light'>
+//                 <Toolbar/>
+//             </ThemeContext.Provider>
+//         );
+//     }
+// }
 
-function Toolbar(props){
-    return(
-        <div>
-            <ThemeButton />
-        </div>
-    );
-}
+// function Toolbar(props){
+//     return(
+//         <div>
+//             <ThemeButton />
+//         </div>
+//     );
+// }
 
-class ThemeButton extends React.Component{
-    static contextType = ThemeContext;
-    render(){
-        return <Button theme={this.context} />;
-    }
-}
+// class ThemeButton extends React.Component{
+//     static contextType = ThemeContext;
+//     render(){
+//         return <Button theme={this.context} />;
+//     }
+// }
 
-function Button(props){
+function ThemedButton(props){
     if(props.theme === 'light')
     {
         return <button>this is light</button>;
@@ -36,5 +36,25 @@ function Button(props){
         return <button>this is dark</button>;
     }
 }
+
+class App extends React.Component{
+    // static Button = <ThemedButton theme='light' />
+    render(){
+        return(
+            <Toolbar>
+                <ThemedButton theme='dark' />
+            </Toolbar>
+        );
+    }
+}
+
+function Toolbar(props){
+    return(
+        <div>
+            {props.children}
+        </div>
+    );
+}
+
 
 export default App;
